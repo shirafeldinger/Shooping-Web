7ace9cb7ae589d3d37b757a832824e674b39c3a7
Shira Feldinger <shirafeldinger@gmail.com>
2020-07-21 12:58:12 +0300

    Updates

---
+++ precache-manifest.73f422393662bd7f792f0c73b60bbf77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "666975aaf52473695c1752cd6da6855d",
+    "url": "/Shopping-Web/index.html"
+  },
+  {
+    "revision": "ab298c9cab464891f4c8",
+    "url": "/Shopping-Web/static/css/main.3a73c99d.chunk.css"
+  },
+  {
+    "revision": "2b15d76e9e37e7ac8d10",
+    "url": "/Shopping-Web/static/js/2.11420320.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Shopping-Web/static/js/2.11420320.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab298c9cab464891f4c8",
+    "url": "/Shopping-Web/static/js/main.b050118f.chunk.js"
+  },
+  {
+    "revision": "47976b18c2f6ac3f240b",
+    "url": "/Shopping-Web/static/js/runtime-main.78dbab38.js"
+  }
+]);
